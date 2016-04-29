5c983c071bf4294647a7219ceb123ec261dbae91
Mirko Klemm <mirko@cm-klemm.de>
2016-04-29 20:44:38 +0200

    Added Unit Test

--- test/src/test/java/net/codesup/jaxb/plugins/expression/ExpressionPluginTest.java
+++ test/src/test/java/net/codesup/jaxb/plugins/expression/ExpressionPluginTest.java
@@ -1,6 +1,9 @@
 package net.codesup.jaxb.plugins.expression;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import net.codesup.jaxb.plugins.expression.test.GlobalDefaultrefSingle;
 
 /**
  * Created by klemm0 on 2016-04-29.
@@ -8,6 +11,11 @@ import org.junit.Test;
 public class ExpressionPluginTest {
 	@Test
 	public void testGlobalDefaultrefSingle() throws Exception {
-
+		GlobalDefaultrefSingle r = new GlobalDefaultrefSingle();
+		r.setAutoLogon(true);
+		r.setUserId("klemm0");
+		r.setUserFirstName("Mirko");
+		r.setUserLastName("Klemm");
+		Assert.assertEquals("klemm0 - Klemm, Mirko", r.toString());
 	}
 }
